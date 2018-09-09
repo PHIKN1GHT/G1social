2b7006e95a1663a1379dbba2ec347b978f2adecb
HicoderDR <36002055+HicoderDR@users.noreply.github.com>
2018-09-09 10:16:23 +0800

    2.9
    cs的天下

--- app/javascript/navigator.vue
+++ app/javascript/navigator.vue
@@ -2,7 +2,7 @@
 	<div id="navigator" style="height:50px">
 		<nav class="navbar navbar-default container-fluid" role="navigation">
 			<a class="navbar-brand" href="#">G1social</a>
-        	<p class="navbar-text">华东理工大学</p>
+        	<p class="navbar-text">华东理工大学是陈实的天下</p>
         </nav>
 	</div>
 </template>
