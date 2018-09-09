6878a475a62e98a1f303e24f464b5f2925aa6ecb
HicoderDR <36002055+HicoderDR@users.noreply.github.com>
2018-09-09 10:21:09 +0800

    1.0
    navigitor

--- app/javascript/navigator.vue
+++ app/javascript/navigator.vue
@@ -2,7 +2,11 @@
 	<div id="navigator" style="height:50px">
 		<nav class="navbar navbar-default container-fluid" role="navigation">
 			<a class="navbar-brand" href="#">G1social</a>
+<<<<<<< HEAD
+        	<p class="navbar-text">华东理工大学是陈实的天下</p>
+=======
         	<p class="navbar-text">华东理工大学奉贤校区</p>
+>>>>>>> parent of 61211bf... 1.0
         </nav>
 	</div>
 </template>
